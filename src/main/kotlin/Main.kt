79a074bc751a92223066b85bdd1674a34cbe92e5
BrayronLeiva <brayronleivas@gmail.com>
2025-02-22 12:46:44 -0600

    Agregacion de la clase CalculatorTest
    Se agrego la clase CalculatorTest, junto con mas pruebas en el main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,9 @@ package org.example
 //Author: Brayron Leiva
 fun main() {
     val calculator = Calculator()
-    println(calculator.add(1.0,1.0))
+    val sum = calculator.add(5.0, 3.0)      // Returns 8.0
+    val difference = calculator.subtract(10.0, 4.0)  // Returns 6.0
+    val product = calculator.multiply(2.0, 3.0)    // Returns 6.0
+    val quotient = calculator.divide(10.0, 2.0)    // Returns 5.0
 
 }
---
+++ src/test/kotlin/CalcultatorTest.kt
@@ -0,0 +1,14 @@
+import org.example.Calculator
+import org.junit.jupiter.api.BeforeEach
+
+class CalcultatorTest {
+    private lateinit var calculator: Calculator
+
+    @BeforeEach
+    fun setUp() {
+        calculator = Calculator()
+    }
+
+
+
+}
