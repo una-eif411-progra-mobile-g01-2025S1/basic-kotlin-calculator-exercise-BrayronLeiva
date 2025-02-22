6bf71fe0876261fbe29faed301fd1ff7a6382ed8
My name <myemail@gmail.com>
2025-02-22 12:18:30 -0600

    Agregacion de la clase calcultator
    Se agrego la clase Calculator, junto con un primera version de los metodos, ademas se realiza una prueba de add en el main

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,11 @@
+package org.example
+
+class Calculator {
+    fun add(a: Double, b: Double) = a + b
+
+    fun subtract(a: Double, b: Double) = a - b
+
+    fun multiply(a: Double, b: Double) = a * b
+
+    fun divide(a: Double, b: Double) = a / b
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,8 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Author: Brayron Leiva
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val calculator = Calculator()
+    println(calculator.add(1.0,1.0))
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
 }
